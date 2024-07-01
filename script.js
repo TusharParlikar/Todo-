e99a6a9a0545b51e8467946f3a81bf27c1e77d1f
Tushar <tparlikar4972gmail.com>
2024-07-02 04:14:14 +0530

    First update is Here

---
+++ script.js
@@ -0,0 +1,13 @@
+let task=document.querySelector(".task");
+let dis=document.querySelector(".dis");
+let button=document.querySelector(".button");
+let mark=document.querySelector(".mark");
+
+button.addEventListener("click",function(){
+
+})//for adding a task.
+
+
+button.addEventListiner("click",function(){
+
+})//for marking a task as completed.
